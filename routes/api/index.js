f5bc3805b2bfd36a7aa22bfedf33e242641da1d6
Tucker Killian <tuckerkillian@Tuckers-MacBook-Pro.local>
2023-12-07 21:47:50 -0600

    finished2

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const userroutes = require('./userRoutes');
-const thoughtroutes = require('./thoughtRoutes');
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
 
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
